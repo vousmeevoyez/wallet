dc3042475ab28a3b3cf68ea23ba044eb9cb10027
Kelvin <kelvindsmn@gmail.com>
2019-02-17 18:21:13 +0700

    fixed async issue

---
+++ dockerfiles/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE db_wallet_dev;
+GRANT ALL PRIVILEGES ON DATABASE db_wallet_dev TO modana;
+CREATE DATABASE db_wallet_prod;
+GRANT ALL PRIVILEGES ON DATABASE db_wallet_prod TO modana;
+CREATE DATABASE db_wallet_testing;
+GRANT ALL PRIVILEGES ON DATABASE db_wallet_testing TO modana;
+
