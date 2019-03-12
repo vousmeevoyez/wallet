dbf1b2eeb600461eb537f674d41c6939695c4a3d
Kelvin <kelvindsmn@gmail.com>
2019-03-12 14:48:16 +0700

    tidy some folder

---
+++ scripts/backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# docker exec -t db-container pg_dumpall -c -U postgres > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+docker exec -t modana_wallet_postgres_1 -c -U modana > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
---
+++ scripts/restore.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#cat dump_name.sql | docker exec -i db-container psql -U modana 
